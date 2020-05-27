d06b5d48e04fd0957545561b9b86ca028844edfa
Abdelrahman995 <44041002+Abdelrahman995@users.noreply.github.com>
2020-05-27 02:23:04 +0200

    Update TwitterConfiguration.java

--- Kafka_Twitter_Producer_Project/src/main/java/ml/dhoomilsheta/app/config/TwitterConfiguration.java
+++ Kafka_Twitter_Producer_Project/src/main/java/ml/dhoomilsheta/app/config/TwitterConfiguration.java
@@ -1,10 +1,10 @@
 package ml.dhoomilsheta.app.config;
 
 public class TwitterConfiguration {
-    public static final String CONSUMER_KEY = "pRbUZHm9o3ibR4PRHvlmGBIpR";
-    public static final String CONSUMER_SECRET = "BJP57je8YLu2qgUqC1JiTsy44RdtOBbrAcTcxdb1V9lijmC35n";
-    public static final String ACCESS_TOKEN = "1116378779885215745-zSZVsTizOiS7cb7ks65pMbfB8ov9cc";
-    public static final String TOKEN_SECRET = "LYgUvyZtUTOuT9swrlSMOrjt16ed7pWCXjColvGe1x2A2";
+    public static final String CONSUMER_KEY = "YOUR CONSUMER KEY";
+    public static final String CONSUMER_SECRET = "YOUR CONSUMER KEY SECRET";
+    public static final String ACCESS_TOKEN = "ACCESS TOKEN";
+    public static final String TOKEN_SECRET = "ACCESS TOKEN SECRET";
     public static final String HASHTAG = "covid19";
 }
 
